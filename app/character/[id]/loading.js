84870e2f146170fffb181f333b8e562ece41d11c
AmjadOka <amjad392q@gmail.com>
2025-12-03 19:41:44 +0200

    Delete app

--- app/character/[id]/loading.js
+++
@@ -1,5 +0,0 @@
-import CharacterCardSkeletonLoading from "@/app/components/CharactarcardLoading";
-
-export default function loading() {
-  return <CharacterCardSkeletonLoading />;
-}
--- app/components/SearchLoading.js
+++
@@ -1,7 +0,0 @@
-export default function SearchBarSkeleton() {
-  return (
-    <div className="w-full flex justify-center mb-6 px-6">
-      <div className="w-full max-w-xl h-12 shimmer rounded-xl bg-black/30 backdrop-blur-lg border border-rm-primary/20"></div>
-    </div>
-  );
-}
